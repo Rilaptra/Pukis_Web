970cf655650ace5991888f89091110d58513364e
Erzy.sh <82298435+Rilaptra@users.noreply.github.com>
2023-10-08 22:45:36 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,15 +1,34 @@
-let menu = document.querySelectorAll(".page");
-let navList = document.querySelectorAll("nav .button a");
-let openmenu = document.getElementById('openmenu')
-let closemenu = document.getElementById('closemenu')
-let nav = document.getElementById('nav')
-let cardbox = document.querySelectorAll('.product .box')
+const menu = document.querySelectorAll(".page");
+const navList = document.querySelectorAll("nav .button a");
+const openmenu = document.getElementById("openmenu");
+const closemenu = document.getElementById("closemenu");
+const nav = document.getElementById("nav");
+const cardbox = document.querySelectorAll(".product .box");
+const pukis = document.getElementById("pukis");
+const kelas = document.getElementById("kelas");
+const special = document.getElementById('special');
+const h1credit = document.querySelector(".credits credit h1");
+alert(window.innerWidth + ' ' + window.innerHeight)
+// HP = 414, 809
+
+if (window.innerWidth < 768) {
+  pukis.innerHTML = "SMANAGA IG";
+  kelas.innerHTML = "Our Class IG";
+}
+
+h1credit.innerHTML = h1credit.innerText
+  .split("")
+  .map(
+    (letter, i) =>
+      `<span style="transition-delay: ${i * 100}ms">${letter}</span>`
+  )
+  .join("");
 
 window.onscroll = () => {
   menu.forEach((page) => {
-    let top = window.scrollY;
-    let offset = page.offsetTop - 100;
-    let height = page.offsetHeight;
+    const top = window.scrollY;
+    const offset = page.offsetTop - 100;
+    const height = page.offsetHeight;
     if (top >= offset && top < offset + height) {
       navList.forEach((nav) => {
         if (page.classList[1] == nav.getAttribute("href").substring(1)) {
@@ -27,17 +46,22 @@ window.onscroll = () => {
 
 // Hamburger menu for Mobile
 tooglenav = () => {
-  nav.classList.toggle('clickview')
-}
-document.addEventListener('click', e => {
-  if (!openmenu.contains(e.target) && !closemenu.contains(e.target) && !nav.contains(e.target)) {
-    nav.classList.remove('clickview')
+  nav.classList.toggle("clickview");
+};
+document.addEventListener("click", (e) => {
+  if (
+    !openmenu.contains(e.target) &&
+    !closemenu.contains(e.target) &&
+    !nav.contains(e.target)
+  ) {
+    nav.classList.remove("clickview");
   }
-})
+});
 
-cardbox.forEach(c => {
-  c.addEventListener('click', (e) => {
-    const link = 'https://rilaptra.github.io/Pukis_Web/' + c.getAttribute('link')
+cardbox.forEach((c) => {
+  c.addEventListener("click", (e) => {
+    const link =
+      "https://rilaptra.github.io/Pukis_Web/" + c.getAttribute("link");
     window.location.href = link;
-  })
-})
+  });
+});
